266606ba53556e5c9f99d97efdc44b3a75251fa8
Emily Main <emilymuirmain@gmail.com>
2016-10-31 11:36:06 -0700

    added login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,12 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def show
+    if @user != User.find(session[:user_id])
+      redirect_to users_path
+    end
+  end
+
   def new
     @user = User.new
   end
@@ -10,15 +16,32 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id.to_s
       redirect_to users_path
     else
       render :new
     end
   end
 
+  def  edit
+    if @user != User.find(session[:user_id])
+    redirect_to users_path
+    end
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'users' => 'users#index'
   get 'users/new' => 'users#new', as: :new_user
-  post 'users' => 'users#create'
+  post 'users/' => 'users#create'
+  get 'users/:id' => 'users#show', as: :user
+  get 'users/:id/edit' => 'users#edit', as: :edit_user
+  patch 'users/:id' => 'users#update'
+  delete 'users/:id' => 'users#destroy'
+  
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/login' => 'sessions#destroy'
+
 end
