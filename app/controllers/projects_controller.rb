fa38434e76311e7f1f893c6a8055ed7436b78fad
Emily Main <emilymuirmain@gmail.com>
2016-11-01 15:24:08 -0700

    added users profiles

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -20,7 +20,7 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.find(params[:id])
-    @creator = User.find(@project.creator).name
+    @creator = User.find(@project.creator)
   end
 
   private
