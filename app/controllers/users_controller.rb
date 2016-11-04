d18e83544058142363298ea647710b9ed2305e21
Emily Main <emilymuirmain@gmail.com>
2016-11-03 18:30:24 -0700

    fixing things

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,7 +50,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation,:image_file_name, skill_ids: [])
+    params.require(:user).permit(:name, :email, :password, :password_confirmation,:image, skill_ids: [])
   end
   def set_user
     @user = User.find(params[:id])
